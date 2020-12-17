8aca23792a7065f1a987433630b10b87ab7f5274
Alex Shomali <alexshomali123@gmail.com>
2020-12-17 17:32:44 +0800

    BST

---
+++ TreesAndGraphs/BTandBST/BST.py
@@ -0,0 +1,139 @@
+# Binary Search Tree (left less than equal to root and right greater than root)
+import QueueLinkedList as queue
+
+# Create node class
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+# insertNode (check/traverse depending on if nodeValue is better or smaller to decide to go left or right; recursively)
+def insertNode(rootNode, nodeValue):
+    if rootNode.data == None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.rightChild, nodeValue)
+    return "The node has been successfully inserted"
+
+# preOrderTraversal (Same as BT)
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+# inOrderTraversal (Same as BT)
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+# postOrderTraversal (Same as BT)
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+
+# levelOrderTraversal (Same as BT)
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            print(root.value.data)
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+            
+# searchNode (check/traverse depending on if nodeValue is better or smaller to decide to go left or right)
+def searchNode(rootNode, nodeValue):
+    if rootNode.data == nodeValue:
+        print("The value is found")
+    elif nodeValue < rootNode.data:
+        if rootNode.leftChild.data == nodeValue:
+            print("The value is found")
+        else:
+            searchNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild.data == nodeValue:
+            print("The value is found")
+        else:
+            searchNode(rootNode.rightChild, nodeValue)
+
+# minValueNode (helper function, to left because left is smaller values)
+def minValueNode(bstNode):
+    current = bstNode
+    while (current.leftChild is not None):
+        current = current.leftChild
+    return current
+
+# deleteNode (3 cases considered in code, traverse by comparing value to left and right [i.e left smaller and right bigger])
+# Case 1 : node to be deleted in leaf
+# Case 2 : node to be deleted has child node
+# Case 3 : node to be deleted has two children
+# (replace with next biggest suitable candidate)
+def deleteNode(rootNode, nodeValue):
+    if rootNode is None:
+        return rootNode
+    if nodeValue < rootNode.data:
+        rootNode.leftChild = deleteNode(rootNode.leftChild, nodeValue)
+    elif nodeValue > rootNode.data:
+        rootNode.rightChild = deleteNode(rootNode.rightChild, nodeValue)
+    else:
+        if rootNode.leftChild is None:
+            temp = rootNode.rightChild
+            rootNode = None
+            return temp
+        
+        if rootNode.rightChild is None:
+            temp = rootNode.leftChild
+            rootNode = None
+            return temp
+
+        # replace with its successor
+        temp = minValueNode(rootNode.rightChild)
+        rootNode.data = temp.data 
+        rootNode.rightChild = deleteNode(rootNode.rightChild, temp.data)
+    return rootNode
+
+# deleteBST
+def deleteBST(rootNode):
+    rootNode.data = None
+    rootNode.leftChild = None
+    rootNode.rightChild = None
+    return "The BST has been successfully deleted"
+
+
+# Trial
+
+newBST = BSTNode(None)
+insertNode(newBST, 70)
+insertNode(newBST,50)
+insertNode(newBST,90)
+insertNode(newBST, 30)
+insertNode(newBST,60)
+insertNode(newBST,80)
+insertNode(newBST,100)
+insertNode(newBST,20)
+insertNode(newBST,40)
+print(deleteBST(newBST))
+levelOrderTraversal(newBST)
+
